39e5dcf858da53a8c3cc5f301f9b3f904e563f34
Victor Faustino <victormachadofaustino@gmail.com>
2026-07-13 01:05:31 -0300

    ajuste de acesso regional/cidade

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-import { AuthProvider } from "@/app/providers/AuthContext";
-import { PermissionProvider } from "@/app/providers/PermissionContext"; // Explicação: Traz o Provedor Soberano de Portaria para a raiz da RAM.
+import { AuthProvider } from "./app/providers/AuthContext";
+import { PermissionProvider } from "./app/providers/PermissionContext"; // Explicação: Traz o Provedor Soberano de Portaria para a raiz da RAM.
 import { registerSW } from "virtual:pwa-register";
 
 if (import.meta.env.PROD) {
